79177c612807bb8a197babdecf274340b9ee16dd
Anas2018EMI <anassaatef@gmail.com>
2024-06-21 11:17:23 -0700

    Initial commit

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -18,10 +18,10 @@ int main(void)
 	else if (n == 0)
 	{
 		printf("%i is zero\n", n);
-    	}
-    	else
-    	{
-        	printf("%i is negative\n", n);
-    	}
+	}
+	else
+	{
+		printf("%i is negative\n", n);
+	}
 	return (0);
 }

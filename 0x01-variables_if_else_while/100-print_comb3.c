d7ce80bba3275654190a082d12df3cc25bdb39e7
Anas2018EMI <anassaatef@gmail.com>
2024-06-21 14:07:41 -0700

    Initial commit

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int c, n;
+
+	for (c = 0; c <= 9; c++)
+	{
+		for (n=0; n <= 9; n++)
+		{
+			if ( c < n )
+			{
+				putchar(c + 48);
+				putchar(n + 48);
+				if (c != 8 || n != 9)
+				{
+					putchar(44);
+					putchar(32);
+				}
+			}
+		}
+	}
+
+	putchar(10);
+	return (0);
+
+}

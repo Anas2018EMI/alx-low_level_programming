f85e542269430496690c6cf8791981fde638ce63
Anas2018EMI <anassaatef@gmail.com>
2024-06-21 12:39:24 -0700

    Initial commit

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char c = 97;
+
+	for (c = 97; c <= 'z'; c++)
+	{
+		putchar(c);
+	}
+
+	for (c = 65; c <= 'Z'; c++)
+        {
+                putchar(c);
+        }
+
+	putchar(10);
+	return (0);
+
+}

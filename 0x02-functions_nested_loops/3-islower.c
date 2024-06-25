98a2d45b78a3539019b16811b629d0b9bcfed0c4
Anas2018EMI <anassaatef@gmail.com>
2024-06-25 08:54:02 -0700

    Initial commit

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,17 +3,17 @@
 /* betty style doc for function _islower goes there */
 /**
  * _islower - Entry point
- *
+ *@c: First operand
  * Return: Always 0 (Success)
  */
 int _islower(int c)
 {
 	if (c >= 97 && c <= 122)
 	{
-		return(1);
+		return (1);
 	}
 	else
 	{
-		return(0);
+		return (0);
 	}
 }

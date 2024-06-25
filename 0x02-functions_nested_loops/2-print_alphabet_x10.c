0ca8931b3c2719de07a81817fd4cf9a5e11d89d8
Anas2018EMI <anassaatef@gmail.com>
2024-06-25 07:49:10 -0700

    Initial commit

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -10,15 +10,14 @@ void print_alphabet_x10(void)
 {
 	int j=0, i = 97;
 
-	do
-	{
+	do {
 		while (i < 123)
-			{
-				_putchar(i);
-				i++;
-			}
+		{
+			_putchar(i);
+			i++;
+		}
 		_putchar('\n');
-		i=97;
+		i = 97;
 		j++;
 	} while (j < 10);
 }

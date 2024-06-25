085884971e990efb2eb34f110b7cc50013bcc33a
Anas2018EMI <anassaatef@gmail.com>
2024-06-25 12:34:27 -0700

    Initial commit

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/* betty style doc for function add goes there */
+/**
+ * add - Entry point
+ * @m: First operand
+ * @n: Second operand
+ *
+ * Return: Always 0 (Success)
+ */
+int add(int m, int n)
+{
+	return (m + n);
+}

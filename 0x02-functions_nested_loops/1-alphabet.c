339fff9809e1f0a2aa07ad998e6ab27df9e6341c
Anas2018EMI <anassaatef@gmail.com>
2024-06-25 05:54:46 -0700

    Initial commit

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,10 +1,15 @@
 #include "main.h"
 
+/* betty style doc for function print_alphabet goes there */
+/**
+ * print_alphabet - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 void print_alphabet(void)
 {
 	int i = 97;
-	
-	while ( i < 123)
+	while (i < 123)
 	{
 		_putchar(i);
 		i++;

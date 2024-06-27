26513144b926b89fc75ecbcc1470a44ef239301c
Anas2018EMI <anassaatef@gmail.com>
2024-06-27 04:14:01 -0700

    Initial commit

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/* betty style doc for function mul goes there */
+/**
+ * mul - Entry point
+ * @a: first arg
+ * @b: second arg
+ *
+ * Return: Always 0 (Success)
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}

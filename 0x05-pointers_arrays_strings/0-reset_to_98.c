1643129dc1769ccf9494869add820ba0405de956
Anas2018EMI <anassaatef@gmail.com>
2024-07-01 09:21:34 -0700

    Initial commit

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+
+/* betty style doc for function reset_to_98 goes there */
+/**
+ * reset_to_98 - Entry point
+ *
+ * @n: first arg
+ * Return: Always 0 (Success)
+ */
+
+void reset_to_98(int *n)
+{
+	*n=98;
+}

d1155c404a921cace57aae5c18b8cd44bfbf0997
Anas2018EMI <anassaatef@gmail.com>
2024-07-01 12:56:52 -0700

    Initial commit

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdio.h>
+
+/* betty style doc for function print_array goes there */
+/**
+ * print_array - Entry point
+ *
+ * @a: first arg
+ * @n: second arg
+ * Return: Always 0 (Success)
+ */
+
+void print_array(int *a, int n)
+{
+	int counter = 0;
+
+	while (counter < n)
+	{
+		if (counter == n-1)
+		{
+			printf("%i", *(a + counter));
+		}
+		else
+		{
+			printf("%i, ", *(a + counter));
+		}
+		counter++;
+		
+	}
+	printf("\n");
+
+}

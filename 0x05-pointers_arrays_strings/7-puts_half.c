a63b530dcbe74760413cc10fd96809e45c220200
Anas2018EMI <anassaatef@gmail.com>
2024-07-01 12:39:26 -0700

    Initial commit

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+
+/* betty style doc for function puts_half goes there */
+/**
+ * puts_half - Entry point
+ *
+ * @str: first arg
+ * Return: Always 0 (Success)
+ */
+
+void puts_half(char *str)
+{
+	int counter = 0;
+	int n, i;
+
+	while (*(str + counter) != '\0')
+	{
+		counter++;
+	}
+	if (counter % 2 == 0)
+	{
+		n = counter / 2;
+	}
+	else
+	{
+		n = (counter - 1) / 2;
+	}
+	i = n;
+	while (i < counter)
+	{
+		_putchar(*(str + i));
+		i++;
+	}
+	_putchar('\n');
+
+}

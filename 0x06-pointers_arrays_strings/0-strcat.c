7bc0189b0bfe85421d2e2ef584c312ce04032c55
Anas2018EMI <anassaatef@gmail.com>
2024-07-03 10:51:58 -0700

    Initial commit

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+
+/* betty style doc for function _strcat goes there */
+/**
+ * _strcat - Entry point
+ *
+ * @src: first arg
+ * @dest: second arg
+ * Return: dest
+ */
+char *_strcat(char *dest, char *src)
+{
+	int counter = 0, i = 0;
+
+	while (*(dest + counter) != '\0')
+	{
+		counter++;
+	}
+	while (*(src + i) != '\0')
+	{
+		*(dest + counter) = *(src + i);
+		counter++;
+		i++;
+	}
+
+	return (dest);
+}

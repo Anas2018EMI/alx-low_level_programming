97af25f3f49c4c67179dcf1bf351fc28ca80a377
Anas2018EMI <anassaatef@gmail.com>
2024-07-04 05:14:53 -0700

    Initial commit

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdio.h>
+
+/* betty style doc for function leet goes there */
+/**
+ * leet - Entry point
+ *
+ * @str: first arg
+ * Return: str
+ */
+char *leet(char *str)
+{
+	int counter = 0, i = 0;
+	char *sep = "aAeEoOtTlL";
+	char *rep = "4433007711";
+
+	while (*(str + counter) != '\0')
+	{
+		i = 0;
+		while (*(sep + i) != '\0')
+		{
+			if (*(str + counter) == *(sep + i))
+			{
+				*(str + counter) = *(rep + i);
+			}
+			i++;
+		}
+		counter++;
+	}
+
+	return (str);
+}

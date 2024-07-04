a72ced7fe456f69222af2441e4190178c3f38499
Anas2018EMI <anassaatef@gmail.com>
2024-07-04 03:21:58 -0700

    Initial commit

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -11,19 +11,23 @@ void reverse_array(int *a, int n)
 {
 	int m, limit, i = 0;
 
-	if (n % 2 != 0)
+	if (n != 1)
 	{
-		limit = (n - 1) / 2;
+		if (n % 2 != 0)
+		{
+			limit = (n - 1) / 2;
+		}
+		else
+		{
+			limit = n / 2;
+		}
+		while (i <= limit)
+		{
+			m = *(a + i);
+			*(a + i) = *(a + n - 1 - i);
+			*(a + n - 1 - i) = m;
+			i++;
+		}
 	}
-	else
-	{
-		limit = n / 2;
-	}
-	while (i <= limit)
-	{
-		m = *(a + i);
-		*(a + i) = *(a + n - 1 - i);
-		*(a + n - 1 - i) = m;
-		i++;
-	}
+
 }

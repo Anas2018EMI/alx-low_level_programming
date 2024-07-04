dad810a95f21bb379616f1307a4392b85f8d0500
Anas2018EMI <anassaatef@gmail.com>
2024-07-04 04:33:28 -0700

    Initial commit

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -11,22 +11,18 @@
 char *cap_string(char *str)
 {
 	int counter = 0, i = 0;
-	char sep[] = {' ', '\t', '\n', ',', ';', '.', '!', '?', '"', '(', ')', '{', '}'};
+	char *sep = " \t\n,;.!?\"(){}";
 
 	while (*(str + counter) != '\0')
 	{
 		i = 0;
-		while (i < 13)
+		while (*(sep + i) != '\0')
 		{
 			if (*(str + counter) == *(sep + i))
 			{
 				if (*(str + counter + 1) >= 97 && *(str + counter + 1) <= 122)
 				{
 					*(str + counter + 1) -= 32;
-					if (*(str + counter) == '\t')
-					{
-						*(str + counter) = ' ';
-					}
 				}
 			}
 			i++;

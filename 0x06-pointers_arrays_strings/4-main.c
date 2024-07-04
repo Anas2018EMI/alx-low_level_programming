bedc0e89d21bacfa137bfc77c2a6bc71610b4071
Anas2018EMI <anassaatef@gmail.com>
2024-07-04 03:56:28 -0700

    Initial commit

--- 0x06-pointers_arrays_strings/4-main.c
+++ 0x06-pointers_arrays_strings/4-main.c
@@ -10,19 +10,19 @@
  */
 void print_array(int *a, int n)
 {
-    int i;
+	int i;
 
-    i = 0;
-    while (i < n)
-    {
-        if (i != 0)
-        {
-            printf(", ");
-        }
-        printf("%d", a[i]);
-        i++;
-    }
-    printf("\n");
+	i = 0;
+	while (i < n)
+	{
+		if (i != 0)
+		{
+			printf(", ");
+		}
+		printf("%d", a[i]);
+		i++;
+	}
+	printf("\n");
 }
 
 /**
@@ -32,11 +32,20 @@ void print_array(int *a, int n)
  */
 int main(void)
 {
-    /*int a[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 98, 1024, 1337};*/
-	int a[]={4, 55};
+	int a[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 98, 1024, 1337};
+	int b[]={4, 55}, c[]={1};
 
-    print_array(a, sizeof(a) / sizeof(int));
-    reverse_array(a, sizeof(a) / sizeof(int));
-    print_array(a, sizeof(a) / sizeof(int));
-    return (0);
+	print_array(a, sizeof(a) / sizeof(int));
+	reverse_array(a, sizeof(a) / sizeof(int));
+	print_array(a, sizeof(a) / sizeof(int));
+
+        print_array(b, sizeof(b) / sizeof(int));
+        reverse_array(b, sizeof(b) / sizeof(int));
+        print_array(b, sizeof(b) / sizeof(int));
+
+
+        print_array(c, sizeof(c) / sizeof(int));
+        reverse_array(c, sizeof(c) / sizeof(int));
+        print_array(c, sizeof(c) / sizeof(int));
+	return (0);
 }

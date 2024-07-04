bf2c38bfc76f66b28339f4e438382bf33258b770
Anas2018EMI <anassaatef@gmail.com>
2024-07-04 05:49:48 -0700

    Initial commit

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/* betty style doc for function rot13 goes there */
+/**
+ * rot13 - Entry point
+ *
+ * @str: first arg
+ * Return: str
+ */
+char *rot13(char *str)
+{
+	int counter = 0, i = 0;
+	char *sep = "abcdefghijklmnopqrstuvwxyz";
+	char *rep = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+	while (*(str + counter) != '\0')
+	{
+		i = 0;
+		while (i < 26)
+		{
+			if (*(str + counter) == *(sep + i))
+			{
+				*(str + counter) = *(sep + ((i + 13) % 26));
+			}
+			else if (*(str + counter) == *(rep + i))
+                        {
+                                *(str + counter) = *(rep + ((i + 13) % 26));
+                        }
+			i++;
+		}
+		counter++;
+	}
+
+	return (str);
+}

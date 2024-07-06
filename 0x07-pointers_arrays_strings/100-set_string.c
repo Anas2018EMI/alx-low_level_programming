002492fba46327da655eed7edd1272ce7d99afd3
Anas2018EMI <anassaatef@gmail.com>
2024-07-06 11:26:15 -0700

    Initial commit

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/* betty style doc for function set_string goes there */
+/**
+ * set_string - Entry point
+ * @s: first arg
+ * @to: second atg
+ *
+ * Return: void
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}

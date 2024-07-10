0b05d935f08e802f205c08dd06ea670331948c95
Anas2018EMI <anassaatef@gmail.com>
2024-07-10 09:57:19 -0700

    Initial commit

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -4,20 +4,19 @@
 /**
  * _puts_recursion - Entry point
  *
- * @src: first arg
+ * @s: first arg
  *
  * Return: str
  */
 void _puts_recursion(char *s)
 {
-	
 	if (*s != '\0')
 	{
 		_putchar(*s);
 		_puts_recursion(s + 1);
 	}
 	else if (*s == '\0')
-        {
+	{
 		_putchar('\n');
 	}
 }

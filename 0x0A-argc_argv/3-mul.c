352430a8fb113357c3897d0d0da02b5c7cf35f32
Anas2018EMI <anassaatef@gmail.com>
2024-07-12 14:11:41 -0700

    Initial commit

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * @argc: first arg
+ * @argv: second arg
+ *
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	int a, b;
+
+	if (argc > 1)
+	{
+		a = atoi(argv[1]);
+		b = atoi(argv[2]);
+		printf("%i\n", a * b);
+	}
+	else if (argc == 1)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	return (0);
+}

1e2a3bd916a3524b7c2960d4cd077036544a662e
Anas2018EMI <anassaatef@gmail.com>
2024-07-16 05:41:08 -0700

    Initial commit

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,39 @@
+#include "main.h"
+#include <stddef.h>
+#include <stdlib.h>
+
+/* betty style doc for function _strdup goes there */
+/**
+ * _strdup - Entry point
+ * @str: first arg
+ *
+ * Return: char *
+ */
+char *_strdup(char *str)
+{
+	unsigned int i = 0, j = 0;
+	char *ptr;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	while (*(str + i))
+        {
+                i++;
+        }
+	ptr = malloc(i + 1);
+	if (ptr == NULL)
+	{
+		return (NULL);
+	}
+
+	while (*str)
+	{
+		*(ptr + j) = *str;
+		str++;
+		j++;
+	}
+	*(ptr + j) = *str;
+	return (ptr);
+}

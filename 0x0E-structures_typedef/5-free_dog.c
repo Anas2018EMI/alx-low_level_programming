0496d1f548b0846d502c807cfabd0e01daf5ab7d
Anas2018EMI <anassaatef@gmail.com>
2024-07-22 15:14:21 -0700

    Initial commit

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -1,19 +1,17 @@
 #include "dog.h"
 #include <stdlib.h>
-/* betty style doc for function new_dog goes there */
+/* betty style doc for function free_dog goes there */
 /**
- *
- * free_dog - - Entry point
+ * free_dog -  Entry point
  * @d: Pointer to dog_t structure to free
- * Return: d_t
- *
+ * Return: void
  */
 void free_dog(dog_t *d)
 {
-    if (d != NULL)
-    {
-        free(d->name);
-        free(d->owner);
-        free(d);
-    }
+	if (d != NULL)
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
 }

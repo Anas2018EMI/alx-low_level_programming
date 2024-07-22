00adefc767670d3d6154864b4ac1875b5fe163dd
Anas2018EMI <anassaatef@gmail.com>
2024-07-22 13:36:14 -0700

    Initial commit

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,40 @@
+#include "dog.h"
+#include <stddef.h>
+#include <stdio.h>
+/* betty style doc for function print_dog goes there */
+/**
+ * print_dog - Entry point
+ *
+ * @d: first arg
+ * Return: void
+ */
+void print_dog(struct dog *d)
+{
+	if (d != NULL)
+	{
+		if (d->name == NULL)
+		{
+			printf("Name: (nil)\n");
+		}
+		else
+		{
+			printf("Name: %s\n", d->name);
+		}
+		if (d->age == 0)
+		{
+			printf("Age: (nil)\n");
+		}
+		else
+		{
+			printf("Age: %f\n", d->age);
+		}
+		if (d->owner == NULL)
+		{
+			printf("Owner: (nil)\n");
+		}
+		else
+		{
+			printf("Owner: %s\n", d->owner);
+		}
+	}
+}

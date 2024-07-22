9ea0ed4d1dfadbfe23a3f6bf89471824e03a4853
Anas2018EMI <anassaatef@gmail.com>
2024-07-22 14:59:47 -0700

    Initial commit

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -2,6 +2,7 @@
 #include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 /* betty style doc for function new_dog goes there */
 /**
  * new_dog - Entry point
@@ -9,20 +10,36 @@
  * @name: first arg
  * @age: second arg
  * @owner: third arg
- * Return: dog_t
+ * Return: d_t
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *d;
+	int len1, len2;
 
+	len1 = strlen(name);
+	len2 = strlen(owner);
 	d = malloc(sizeof(dog_t));
 	if (d == NULL)
 	{
 		free(d);
 		return (NULL);
 	}
+	d->name = malloc(sizeof(char) * (len1 + 1));
+	if (d->name == NULL)
+	{
+		free(d);
+		return (NULL);
+	}
+	d->owner = malloc(sizeof(char) * (len2 + 1));
+	if (d->owner == NULL)
+	{
+		free(d->name);
+		free(d);
+		return (NULL);
+	}
 	d->name = name;
-	d->age = age;
 	d->owner = owner;
+	d->age = age;
 	return (d);
 }

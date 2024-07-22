379161285aef67a5d3a38b366561a97676948228
Anas2018EMI <anassaatef@gmail.com>
2024-07-22 12:57:54 -0700

    Initial commit

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,4 +1,5 @@
 #include "dog.h"
+#include <stddef.h>
 /* betty style doc for function init_dog goes there */
 /**
  * init_dog - Entry point
@@ -11,7 +12,10 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	d->name = name;
-	d->age = age;
-	d->owner = owner;
+	if (d != NULL)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
 }

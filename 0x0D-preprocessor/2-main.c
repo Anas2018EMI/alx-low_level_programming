8b5f2719e36206e8c26c5f947619b96c78b62842
Anas2018EMI <anassaatef@gmail.com>
2024-07-22 10:33:46 -0700

    Initial commit

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Return: int
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}

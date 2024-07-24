a5c0d5737bfc01cdcca1843a543a2eabf031815a
Anas2018EMI <anassaatef@gmail.com>
2024-07-24 14:46:12 -0700

    Initial commit

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+    int i, num_bytes;
+    unsigned char *func_ptr;
+
+    if (argc != 2)
+    {
+        printf("Error\n");
+        exit(1);
+    }
+
+    num_bytes = atoi(argv[1]);
+
+    if (num_bytes < 0)
+    {
+        printf("Error\n");
+        exit(2);
+    }
+
+    func_ptr = (unsigned char *)main;
+
+    for (i = 0; i < num_bytes; i++)
+    {
+        printf("%02x", func_ptr[i]);
+    }
+
+    printf("\n");
+    return (0);
+}

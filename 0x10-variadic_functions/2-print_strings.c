54f972a80a0185a4313c35610b515f1df372f8a5
Anas2018EMI <anassaatef@gmail.com>
2024-07-25 12:14:18 -0700

    Initial commit

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,38 @@
+#include "variadic_functions.h"
+#include <stddef.h>
+#include <stdarg.h>
+#include <stdio.h>
+/* betty style doc for function print_strings goes there */
+/**
+ * print_strings - Entry point
+ *
+ * @separator: first arg
+ * @n: second arg
+ * Return: void
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	va_list to;
+	char *str;
+
+	va_start(to, n);
+	for (i = 0; i < n; i++)
+	{
+		str = va_arg(to, char *);
+		if (str == NULL)
+		{
+			printf("nil");
+		}
+		else
+		{
+			printf("%s", str);
+		}
+		if (separator != NULL && i != n - 1)
+		{
+			printf("%s", separator);
+		}
+	}
+	va_end(to);
+	printf("\n");
+}

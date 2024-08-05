75a20b0bae8f3a4c3e9e2cdb3c1a39c6d657ee60
Anas2018EMI <anassaatef@gmail.com>
2024-08-05 10:11:41 -0700

    Initial commit

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -14,10 +14,15 @@ size_t print_listint(const listint_t *h)
 	size_t i = 0;
 	const listint_t *ptr;
 
-	for (ptr = h; ptr != NULL; ptr = ptr->next)
+	if (h == NULL)
+		i = 0;
+	else
 	{
-		printf("%i\n", ptr->n);
-		i++;
+		for (ptr = h; ptr != NULL; ptr = ptr->next)
+		{
+			printf("%i\n", ptr->n);
+			i++;
+		}
 	}
 	return (i);
 }

5d9036a8fc9689a79e1c5122a641972a853f9291
Anas2018EMI <anassaatef@gmail.com>
2024-08-05 10:07:47 -0700

    Initial commit

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+#include <stddef.h>
+#include <stdlib.h>
+/* betty style doc for function free_listint2 goes there */
+/**
+ * free_listint2 - Entry point
+ *
+ * @head: first arg
+ * Return: void
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *ptr = *head, *ptr2;
+
+	while (ptr != NULL)
+	{
+		ptr2 = ptr->next;
+		free(ptr);
+		ptr = ptr2;
+	}
+	*head = NULL;
+}

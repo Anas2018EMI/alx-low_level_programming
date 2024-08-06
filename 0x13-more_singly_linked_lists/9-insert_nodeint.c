e8ad781b9b051501d17a2d19423e47c1dbed7184
Anas2018EMI <anassaatef@gmail.com>
2024-08-06 11:55:02 -0700

    Initial commit

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -16,7 +16,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *ind, *ptr;
 	unsigned int i = 0;
 
-	if (head == NULL || *head == NULL)
+	if (head == NULL)
 		return (NULL);
 	for (ind = *head; ind != NULL; ind = ind->next)
 	{

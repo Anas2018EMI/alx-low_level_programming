53ca80544fd9bc575b4db90917b2a01091778f0b
Anas2018EMI <anassaatef@gmail.com>
2024-08-06 08:23:54 -0700

    Initial commit

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -10,12 +10,13 @@
  */
 void free_listint2(listint_t **head)
 {
-	listint_t *ptr = *head, *ptr2;
+	listint_t *ptr, *ptr2;
 
 	if (head == NULL)
 	{
 		return;
 	}
+	ptr = *head;
 	while (ptr != NULL)
 	{
 		ptr2 = ptr->next;

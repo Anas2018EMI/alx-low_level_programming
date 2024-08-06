b7cafa880c93965238173bc53992e54663e26b2a
Anas2018EMI <anassaatef@gmail.com>
2024-08-06 12:01:37 -0700

    Initial commit

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -13,25 +13,39 @@
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	listint_t *ind, *ptr;
+	listint_t *new_node, *current;
 	unsigned int i = 0;
 
 	if (head == NULL)
 		return (NULL);
-	for (ind = *head; ind != NULL; ind = ind->next)
+
+	new_node = malloc(sizeof(listint_t));
+	if (new_node == NULL)
+		return (NULL);
+	new_node->n = n;
+
+	if (idx == 0)
 	{
-		if (i == idx)
-		{
-			ptr = malloc(sizeof(listint_t));
-			if (ptr == NULL)
-				return (NULL);
-			ptr->n = ind->n;
-			ptr->next = ind->next;
-			ind->n = n;
-			ind->next = ptr;
-			return (ind);
-		}
+		new_node->next = *head;
+		*head = new_node;
+		return (new_node);
+	}
+
+	current = *head;
+	while (current != NULL && i < idx - 1)
+	{
+		current = current->next;
 		i++;
 	}
-	return (NULL);
+
+	if (current == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
+
+	new_node->next = current->next;
+	current->next = new_node;
+
+	return (new_node);
 }

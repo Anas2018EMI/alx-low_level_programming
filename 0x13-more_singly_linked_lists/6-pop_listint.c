a0f796c5e80d41a3ce3846e5eaf4697e3cfc425c
Anas2018EMI <anassaatef@gmail.com>
2024-08-06 10:31:24 -0700

    Initial commit

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+#include <stddef.h>
+#include <string.h>
+#include <stdlib.h>
+/* betty style doc for function  pop_listint goes there */
+/**
+ *  pop_listint- Entry point
+ *
+ * @head: first arg
+ * Return: int
+ */
+int pop_listint(listint_t **head)
+{
+	listint_t *ptr;
+	int i;
+
+	if (head == NULL)
+	{
+		return (0);
+	}
+	ptr = *head;
+	i = (*head)->n;
+	*head = (*head)->next;
+	free(ptr);
+	return (i);
+}

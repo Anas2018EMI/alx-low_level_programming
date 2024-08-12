ee3d6cdf54a8581737eefedff5aec47ee954fb2a
Anas2018EMI <anassaatef@gmail.com>
2024-08-12 10:35:00 -0700

    Initial commit

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,40 @@
+#include "main.h"
+#include <stddef.h>
+#include <fcntl.h>
+#include <unistd.h>
+/* betty style doc for function read_textfile goes there */
+/**
+ * read_textfile - Entry point
+ *
+ * @filename: first arg
+ * @letters: second arg
+ * Return: ssize_t
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	ssize_t fdes, result, result1;
+	char *c;
+
+	if (filename == NULL)
+	{
+		return (0);
+	}
+	fdes = open(filename, O_RDONLY);
+	if (fdes == -1)
+	{
+		return (0);
+	}
+	c = malloc(sizeof(char) * letters);
+	result = read(fdes, c, letters);
+	if (result == -1)
+	{
+		return (0);
+	}
+	result1 = write(1, c, letters);
+	if (result1 < (ssize_t) letters)
+	{
+		return (0);
+	}
+
+	return (result);
+}
---
+++ 0x15-file_io/main.h
@@ -0,0 +1,8 @@
+#ifndef VAR
+#define VAR
+#include <stddef.h>
+#include <sys/types.h>
+#include <stdlib.h>
+int _putchar(char c);
+ssize_t read_textfile(const char *filename, size_t letters);
+#endif
